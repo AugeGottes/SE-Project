b2b801b8dbc18b9d903e41de56d6d79b9881d0c8
Debanjan Das <debanjandasrise@gmail.com>
2022-11-27 14:27:36 +0530

    third commit

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
                 {
                     if(password_verify($password, $hashed_password))
                     {
-                        // this means the password is corrct. Allow user to login
+
                         session_start();
                         $_SESSION["username"] = $username;
                         $_SESSION["id"] = $id;
@@ -65,11 +65,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>The login page</title>
